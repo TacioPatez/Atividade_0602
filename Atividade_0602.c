4c7a24d98993db1b182e4b13326aef6e242ef22b
Tácio Patez <taciopatez@hotmail.com>
2025-02-12 00:17:59 -0300

    Final

--- Atividade_0602.c
+++ Atividade_0602.c
@@ -4,51 +4,47 @@
 #include "hardware/irq.h"
 #include "hardware/pwm.h"
 
-#define PWM_SERVO 22
-#define PWM_LED 12
+#define PWM_PIN 12 //LED PINO = 12
 
 const uint16_t WRAP_PERIOD = 20000;
-const float PWM_DIVISER = 104.0; //divisor do clock para o PWM
-uint16_t led_level = 0; //nível inicial do pwm (duty cycle)
-uint16_t servo_level = 0; //nível inicial do pwm (duty cycle)
+const float PWM_DIVISER = 125.0; //divisor do clock para o PWM
+uint16_t pwm_level = 500; //nível inicial do pwm (duty cycle)
 
 int main() {
     stdio_init_all();
     
-    gpio_set_function(PWM_LED, GPIO_FUNC_PWM);
-    gpio_set_function(PWM_SERVO, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_PIN, GPIO_FUNC_PWM);
 
-    uint slice_led = pwm_gpio_to_slice_num(PWM_LED);
-    uint slice_servo = pwm_gpio_to_slice_num(PWM_SERVO);
+    uint slice_servo = pwm_gpio_to_slice_num(PWM_PIN);
 
-    pwm_set_clkdiv(slice_led, PWM_DIVISER); //define o divisor de clock do PWM
     pwm_set_clkdiv(slice_servo, PWM_DIVISER); //define o divisor de clock do PWM
 
-    pwm_set_wrap(slice_led, WRAP_PERIOD); //definir o valor de wrap
     pwm_set_wrap(slice_servo, WRAP_PERIOD); //definir o valor de wrap
 
-    pwm_set_gpio_level(PWM_LED, 0); //definir o cico de trabalho (duty cycle) do pwm
-    pwm_set_gpio_level(PWM_SERVO, 0); //definir o cico de trabalho (duty cycle) do pwm
+    pwm_set_enabled(PWM_PIN, true); //habilita o pwm no slice correspondente
 
-    pwm_set_enabled(PWM_LED, true); //habilita o pwm no slice correspondente
-    pwm_set_enabled(PWM_SERVO, true); //habilita o pwm no slice correspondente
-
-    pwm_set_gpio_level(PWM_SERVO, 2400);
+    pwm_set_gpio_level(PWM_PIN, 2400);
     sleep_ms(5000);
 
-    pwm_set_gpio_level(PWM_SERVO, 1470);
+    pwm_set_gpio_level(PWM_PIN, 1470);
     sleep_ms(5000);
 
-    pwm_set_gpio_level(PWM_SERVO, 500);
+    pwm_set_gpio_level(PWM_PIN, 500);
     sleep_ms(5000);
 
-
+    bool up_down = 1;
 
     
     while (true) {
-        printf("Level: %d\n", servo_level);
-        servo_level += 1;
-        pwm_set_gpio_level(PWM_SERVO, servo_level);
-        sleep_ms(5);
+        sleep_ms(10);
+        if (up_down){
+            pwm_level += 5;
+        } else {
+            pwm_level -= 5;
+        }
+        pwm_set_gpio_level(PWM_PIN, pwm_level);
+        if(pwm_level > 2400 || pwm_level < 500) {
+            up_down = !up_down;
+        }
     }
 }
